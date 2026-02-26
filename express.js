b2946a9a338f11213a41db4fc742d78cd402c80f
Jeremiah-ET <techmartbusinessservice@gmail.com>
2026-02-26 17:30:43 -0500

    started ejs format need to shift over fully

--- express.js
+++ express.js
@@ -16,10 +16,12 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.render("home", { name: "Jeremiah" });
 });
 
 // Start server
